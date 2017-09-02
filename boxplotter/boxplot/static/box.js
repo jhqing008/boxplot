2374a83db21899b2ed637d5f5b584736ea1df0ae
jack ouyang <ouyan008@gmail.com>
2017-09-02 14:11:28 -0700

    update boxplot with new features legend

--- boxplotter/boxplot/static/box.js
+++ boxplotter/boxplot/static/box.js
@@ -1,132 +1,258 @@
 var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    svg_width = +svg.attr("width"),
+    svg_height = +svg.attr("height"),
+    margin = {top: 70, right: 20, bottom: 20, left: 30},
     width = +svg.attr("width") - (margin.left + margin.right),
-    height = +svg.attr("height") - (margin.top + margin.bottom);
+    height = +svg.attr("height") - (margin.top + margin.bottom),
+    tooltip_boxplot = d3.select("body").append("div").attr("id", "tooltip_boxplot").style("opacity", 0);
+    tooltip_outlier = d3.select("body").append("div").attr("id", "tooltip_outlier").style("opacity", 0);
 
-var tooltip = d3.select("body").append("div")
-    .attr("id", "tooltip")
-    .style("opacity", 0);
-
-var x = d3.scaleBand().rangeRound([0, width]).padding(0.5),
-    y = d3.scaleLinear().rangeRound([height, 0]);
-
-var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-var all_data_values = []
+var extreme_values = [];
 for(var i = 0; i < data.length; i++) {
-    all_data_values = all_data_values.concat(data[i].whiskers, data[i].outliers);
+    extreme_values = extreme_values.concat(data[i].whiskers, data[i].outliers);
 }
 
-x.domain(data.map(function(d){ return d.label;}));
-y.domain([d3.min(all_data_values) - 10, d3.max(all_data_values) + 10]);
+var x = d3.scaleBand().rangeRound([0, width]).padding(0.75).domain(data.map(function(d){ return d.label; }));
+    y = d3.scaleLinear().rangeRound([height, 0]).domain([d3.min(extreme_values) - 10, d3.max(extreme_values) + 10]);
 
-g.append("g")
-    .attr("class", "axis axis-x")
+var boxplot_color = d3.scaleOrdinal(d3.schemeCategory10).domain([+data[0].label, +data[data.length - 1].label]);
+
+var chart_properties = {
+    title: "Shampoo Sales by Year",
+    axis_labels: {x: "year", y: "sales"}
+}
+
+var canvas_background = svg.append("rect")
+    .attr("class", "background")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", +svg.attr("width"))
+    .attr("height", +svg.attr("height"));
+
+var title = svg.append("text").text(chart_properties.title)
+    .attr("x", svg_width/2)
+    .attr("y", 30)
+    .attr("class", "title");
+
+// Build Legend.
+var legend = {
+    canvas: svg.append("g"),
+    location: {x: width-110, y: 20},
+    dimensions: {width: 110, height: 90},
+    contents: {}
+};
+
+legend.contents.background = legend.canvas.append("rect");
+legend.contents.title = legend.canvas.append("text").text("Legend");
+
+legend.contents.median = {canvas: legend.canvas.append("g"), contents: {}};
+legend.contents.median.contents.indicator_line = legend.contents.median.canvas.append("line");
+legend.contents.median.contents.indicator_circle = legend.contents.median.canvas.append("circle");
+legend.contents.median.contents.title = legend.contents.median.canvas.append("text").text("Median");
+
+legend.contents.whiskers = {canvas: legend.canvas.append("g"), contents: {}};
+legend.contents.whiskers.contents.indicator_line = legend.contents.whiskers.canvas.append("line");
+legend.contents.whiskers.contents.title = legend.contents.whiskers.canvas.append("text").text("Whiskers");
+
+legend.contents.outliers = {canvas: legend.canvas.append("g"), contents: {}};
+legend.contents.outliers.contents.indicator_circle = legend.contents.outliers.canvas.append("circle");
+legend.contents.outliers.contents.title = legend.contents.outliers.canvas.append("text").text("Outliers");
+
+legend.canvas
+    .attr("transform", "translate(" + legend.location.x + "," + legend.location.y + ")")
+    .attr("class", "legend");
+
+legend.contents.background
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", legend.dimensions.width)
+    .attr("height", legend.dimensions.height)
+    .attr("class", "background");
+
+legend.contents.title
+    .attr("x", legend.dimensions.width/2)
+    .attr("y", 15)
+    .attr("text-anchor", "middle");
+
+legend.contents.median.contents.indicator_line
+    .attr("x1", 5)
+    .attr("y1", 35)
+    .attr("x2", 30)
+    .attr("y2", 35)
+    .attr("class", "median-line");
+
+legend.contents.median.contents.indicator_circle
+    .attr("cx", 17)
+    .attr("cy", 35)
+    .attr("class", "median-circle");
+
+legend.contents.median.contents.title
+    .attr("x", 35)
+    .attr("y", 35)
+    .attr("dy", "0.3em");
+
+legend.contents.whiskers.contents.indicator_line
+    .attr("x1", 5)
+    .attr("y1", 55)
+    .attr("x2", 30)
+    .attr("y2", 55)
+    .attr("class", "whisker-line");
+
+legend.contents.whiskers.contents.title
+    .attr("x", 35)
+    .attr("y", 55)
+    .attr("dy", "0.3em");
+
+legend.contents.outliers.contents.indicator_circle
+    .attr("cx", 17)
+    .attr("cy", 75)
+    .style("fill", "rgb(255, 255, 255)")
+    .attr("class", "outlier-circle");
+
+legend.contents.outliers.contents.title
+    .attr("x", 35)
+    .attr("y", 75)
+    .attr("dy", "0.3em");
+
+var chart = {canvas: svg.append("g"), contents: {}};
+
+// Build Chart Axis.
+chart.contents.x_label = chart.canvas.append("text");
+chart.contents.y_label = chart.canvas.append("text");
+
+chart.canvas.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+chart.contents.x_label
+    .attr("x", width)
+    .attr("y", height - 6)
+    .attr("class", "x-label")
+    .text(chart_properties.axis_labels.x);
+
+chart.contents.y_label
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("dy", ".85em")
+    .attr("transform", "rotate(-90)")
+    .attr("class", "y-label")
+    .text(chart_properties.axis_labels.y);
+
+chart.contents.x_axis = chart.canvas.append("g")
     .attr("transform", "translate(0, " + height + ")")
     .call(d3.axisBottom(x));
 
-g.append("g")
-    .attr("class", "axis axis-y")
+chart.contents.y_axis = chart.canvas.append("g")
     .call(d3.axisLeft(y).ticks(10));
 
-g.selectAll(".upperwhiskers")
-    .data(data)
-    .enter().append("line")
-    .attr("class", "whisker")
-    .attr("x1", function(d) { return x(d.label) + (0.15 * x.bandwidth()); })
-    .attr("y1", function(d) { return y(d.whiskers[1]); })
-    .attr("x2", function(d) { return x(d.label) + (0.85 * x.bandwidth()); })
-    .attr("y2", function(d) { return y(d.whiskers[1]); })
-    .attr("stroke", "#77dd77")
-    .attr("stroke-width", 2)
-    .attr("shape-rendering", "crispEdges");
-
-g.selectAll(".lowerwhiskers")
-    .data(data)
-    .enter().append("line")
-    .attr("x1", function(d) { return x(d.label) + (0.15 * x.bandwidth()); })
-    .attr("y1", function(d) { return y(d.whiskers[0]); })
-    .attr("x2", function(d) { return x(d.label) + (0.85 * x.bandwidth()); })
-    .attr("y2", function(d) { return y(d.whiskers[0]); })
-    .attr("stroke", "#77dd77")
-    .attr("stroke-width", 2)
-    .attr("shape-rendering", "crispEdges");
-
-g.selectAll(".upperlowerwhiskers")
-    .data(data)
-    .enter().append("line")
-    .attr("x1", function(d) { return x(d.label) + (0.5 * x.bandwidth()); })
-    .attr("y1", function(d) { return y(d.whiskers[0]); })
-    .attr("x2", function(d) { return x(d.label) + (0.5 * x.bandwidth()); })
-    .attr("y2", function(d) { return y(d.whiskers[1]); })
-    .attr("stroke", "#77dd77")
-    .attr("stroke-width", 2)
-    .attr("shape-rendering", "crispEdges");
-
-g.selectAll(".iqr")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", "iqr")
-    .attr("x", function(d) { return x(d.label); })
-    .attr("y", function(d) { return y(d.quartiles[1]); })
-    .attr("width", x.bandwidth())
-    .attr("height", function (d) { return y(d.quartiles[0]) - y(d.quartiles[1]); })
-    .attr('fill', "rgba(255,255,255,1)")
-    .attr('stroke', "#77dd77")
-    .attr("stroke-width", 2)
-    .attr("shape-rendering", "crispEdges")
-    .on("mouseover", function(d) {      
-        tooltip.transition()        
-            .duration(200)   
-            .style("opacity", .9);      
-        tooltip.html("Upper Limit: " + d.whiskers[1] + "<br />75% Quartile: " + d.quartiles[1] + "<br />Median: " + d.median + "<br />25% Quartile: " + d.quartiles[0] + "<br />Lower Limit: " + d.whiskers[0])  
-            .style("left", (d3.event.pageX) + "px")     
-            .style("top", (d3.event.pageY) + "px");    
-        })                  
-    .on("mouseout", function(d) {       
-        tooltip.transition()        
-            .duration(500)      
-            .style("opacity", 0);   
-    });
-
-g.selectAll(".median")
-    .data(data)
-    .enter().append("line")
-    .attr("class", "median")
-    .attr("x1", function(d) { return x(d.label); })
-    .attr("y1", function(d) { return y(d.median); })
-    .attr("x2", function(d) { return x(d.label) + x.bandwidth(); })
-    .attr("y2", function(d) { return y(d.median); })
-    .attr("stroke", "#77dd77")
-    .attr("stroke-width", 2)
-    .attr("shape-rendering", "crispEdges");
-
-for(var i = 1; i < data.length; i++) {
-    g.append("line")
-        .attr("class", "trendlines")
-        .attr("x1", x(data[i-1].label) + (0.5 * x.bandwidth()))
-        .attr("y1", y(data[i-1].median))
-        .attr("x2", x(data[i].label) + (0.5 * x.bandwidth()))
-        .attr("y2", y(data[i].median))
-        .attr("stroke", "#77dd77")
-        .attr("stroke-width", 2)
-        .attr("shape-rendering", "crispEdges");
-}
-
+// Build Boxplots
+chart.contents.boxplots = [];
 for(var i = 0; i < data.length; i++) {
-    for(var j = 0; j < data[i].outliers.length; j++) {
-        g.append("circle")
-            .attr("class", "outlier")
-            .attr("cx", x(data[i].label) + (0.5 * x.bandwidth()))
-            .attr("cy", y(data[i].outliers[j]))
-            .attr("r", 5)
-            .style("fill", "#779ecb");
+    var canvas = chart.canvas;
+    var boxplot_elements = {};
+    var d = data[i];
+
+    boxplot_elements.whisker_upper = canvas.append("line");
+    boxplot_elements.whisker_lower = canvas.append("line");
+    boxplot_elements.whisker_line = canvas.append("line");
+    boxplot_elements.inner_quartile_range = canvas.append("rect");
+    boxplot_elements.median_line = canvas.append("line");
+    boxplot_elements.median_circle = canvas.append("circle");
+
+    boxplot_elements.whisker_upper
+        .attr("class", "whisker-end")
+        .attr("x1", x(d.label) + ~~(0.25 * x.bandwidth()))
+        .attr("y1", y(d.whiskers[1]))
+        .attr("x2", x(d.label) + ~~(0.75 * x.bandwidth()))
+        .attr("y2", y(d.whiskers[1]));
+
+    boxplot_elements.whisker_lower
+        .attr("class", "whisker-end")
+        .attr("x1", x(d.label) + ~~(0.25 * x.bandwidth()))
+        .attr("y1", y(d.whiskers[0]))
+        .attr("x2", x(d.label) + ~~(0.75 * x.bandwidth()))
+        .attr("y2", y(d.whiskers[0]));
+
+    boxplot_elements.whisker_line
+        .attr("class", "whisker-line")
+        .attr("x1", x(d.label) + ~~(0.5 * x.bandwidth()))
+        .attr("y1", y(d.whiskers[0]))
+        .attr("x2", x(d.label) + ~~(0.5 * x.bandwidth()))
+        .attr("y2", y(d.whiskers[1]));
+
+    boxplot_elements.inner_quartile_range
+        .attr("class", "inner-quartile-range")
+        .attr("x", x(d.label))
+        .attr("y", y(d.quartiles[1]))
+        .attr("width", x.bandwidth())
+        .attr("height", y(d.quartiles[0]) - y(d.quartiles[1]))
+        .attr("fill", boxplot_color(d.label))
+        .on("mouseover", function(d) {
+            tooltip_boxplot.transition()
+                .duration(200)
+                .style("opacity", 0.9);
+            tooltip_boxplot.html("Max: " + d.max + "<br />Upper Limit: " + d.whiskers[1] + "<br />75% Quartile: " + d.quartiles[1] + "<br />Median: " + d.median + "<br />25% Quartile: " + d.quartiles[0] + "<br />Lower Limit: " + d.whiskers[0] + "<br />Min: " + d.min + "<br />Std. Deviation: " + d.std_deviation + "<br />Mean: " + d.mean)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY) + "px");
+        }.bind(null, d))
+        .on("mouseout", function(d) {
+            tooltip_boxplot.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
+
+    boxplot_elements.median_line
+        .attr("class", "median-line")
+        .attr("x1", x(d.label) + 1)
+        .attr("y1", y(d.median))
+        .attr("x2", x(d.label) + x.bandwidth() - 1)
+        .attr("y2", y(d.median));
+
+    boxplot_elements.median_circle
+        .attr("class", "median-circle")
+        .attr("cx", x(d.label) + ~~(0.5 * x.bandwidth()))
+        .attr("cy", y(d.median));
+
+    boxplot_elements.outliers = []
+    for(var j = 0; j < d.outliers.length; j++) {
+        var outlier = canvas.append("circle")
+            .attr("class", "outlier-circle")
+            .attr("cx", x(d.label) + ~~(0.5 * x.bandwidth()))
+            .attr("cy", y(d.outliers[j]))
+            .style("fill", boxplot_color(d.label))
+            .on("mouseover", function(d) {
+                tooltip_outlier.transition()
+                    .duration(200)
+                    .style("opacity", 0.9);
+                tooltip_outlier.html(d)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY) + "px");
+            }.bind(null, d.outliers[j]))
+            .on("mouseout", function(d) {
+                tooltip_outlier.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+
+        boxplot_elements.outliers.push(outlier);
     }
 
-    g.append("circle")
-        .attr("class", "trendline_anchor")
-        .attr("cx", x(data[i].label) + (0.5 * x.bandwidth()))
-        .attr("cy", y(data[i].median))
-        .attr("r", 5)
-        .style("fill", "#ffb347");
+    chart.contents.boxplots.push(boxplot_elements);
 }
+
+// Build Trendlines.
+chart.contents.trendlines = []
+for(var i = 1; i < data.length; i++) {
+    var trendline = chart.canvas.append("line")
+        .attr("class", "trendlines")
+        .attr("x1", x(data[i-1].label) + ~~(0.5 * x.bandwidth()))
+        .attr("y1", y(data[i-1].median))
+        .attr("x2", x(data[i].label) + ~~(0.5 * x.bandwidth()))
+        .attr("y2", y(data[i].median));
+
+    chart.contents.trendlines.push(trendline);
+}
+
+d3.select('#exportpng').on('click', function() {
+    svgString2Image(getSVGString(svg.node()), 2*width, 2*height, "png", function(dataBlob, filesize) {
+        saveAs(dataBlob, "visualization_svg.png");
+    });
+});
--- boxplotter/boxplot/views.py
+++ boxplotter/boxplot/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import HttpResponse
 import pandas as pd
 import os
 import json
+import csv
+import io
+import sys
 
 # Create your views here.
-
-def index(request):
-    return render(request, "index.html")
-
-def data(request):
-    data_set = pd.read_csv(os.path.join(os.path.abspath(os.path.dirname(__file__)), "./static/data.csv"))
+def calculate_data():
+    data_set = pd.read_csv(os.path.join(os.path.abspath(os.path.dirname(__file__)), "./static/data_raw.csv"))
     data_set.columns = ['Time', 'Sales']
     data_set['Time'] = pd.to_datetime(data_set['Time'])
     data_set.index = data_set['Time']
@@ -20,6 +19,11 @@ def data(request):
     keys = list(s.groups.keys())
 
     median = s.median()
+    mean = s.mean()
+    std = s.std()
+
+    max_value = s.max()
+    min_value = s.min()
 
     lower_q = s.quantile(0.25)
     upper_q = s.quantile(0.75)
@@ -38,24 +42,51 @@ def data(request):
             if data_set['Year'].iloc[j] == keys[i]:
                 if data_set["Sales"].iloc[j] < lower_limit["Sales"].iloc[i]:
                     outliers[i].append(data_set["Sales"].iloc[j])
-                if data_set["Sales"].iloc[j] > upper_limit["Sales"].iloc[i]:
+                elif data_set["Sales"].iloc[j] > upper_limit["Sales"].iloc[i]:
                     outliers[i].append(data_set["Sales"].iloc[j])
                 else:
                     reg_nums[i].append(data_set["Sales"].iloc[j])
-        whiskers[i] = [min(float(s) for s in reg_nums[i]), max(float(s) for s in reg_nums[i])]
-        
+        whiskers[i] = [round(min(float(s) for s in reg_nums[i]),2), round(max(float(s) for s in reg_nums[i]),2)]
+
     return_data = []
     for i in range(len(keys)):
         return_data.append({
             "label": str(keys[i]),
-            "median": median['Sales'].iloc[i],
+            "std_deviation": round(std['Sales'].iloc[i],2),
+            "min": round(min_value['Sales'].iloc[i],2),
+            "mean": round(mean['Sales'].iloc[i],2),
+            "median": round(median['Sales'].iloc[i],2),
+            "quartiles": [round(lower_q['Sales'].iloc[i],2), round((upper_q['Sales'].iloc[i]),2)],
             "whiskers": whiskers[i],
-            "quartiles" :[lower_q['Sales'].iloc[i],upper_q['Sales'].iloc[i]],
+            "max": round(max_value['Sales'].iloc[i],2),
             "outliers": outliers[i]
         })
+    return return_data
 
+def index(request):
+    return render(request, "index.html")
+
+def data(request):
     context = {
-        "json_data": json.dumps(return_data)
+        "json_data": json.dumps(calculate_data())
     }
     
-    return render(request, "return_data.json", context);
+    return render(request, "return_data.json", context)
+
+def data_csv(request):
+    data_set = calculate_data()
+
+    if sys.version_info < (3,0):
+        csv_data = io.BytesIO()
+    else:
+        csv_data = io.StringIO()
+
+    writer = csv.writer(csv_data, quoting=csv.QUOTE_NONNUMERIC)
+    writer.writerow(["label", "mean", "std. deviation", "min", "lower limit", "25% quartile", "median", "75% quartile", "upper limit", "max"])
+    for i in range(len(data_set)):
+        row = data_set[i]
+        writer.writerow([row["label"], row["mean"], row["std_deviation"], row["min"], row["whiskers"][0], row["quartiles"][0], row["median"], row["quartiles"][1], row["whiskers"][1], row["max"]])
+
+    response = HttpResponse(csv_data.getvalue(), content_type="text/csv")
+    response["Content-Disposition"] = "attachment; filename=\"data.csv\""
+    return response
